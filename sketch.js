fe2a4fd1bb862642ba484808c6603667fc3102ec
Magno Costa <magnocp123@gmail.com>
2020-06-30 23:01:14 -0300

    completo

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  telaInicial = new TelaInicial()
+  jogo = new Jogo()
+  cenas = {
+    telaInicial,
+    jogo
+  }
+  jogo.setup()
+  botaoGerenciador = new BotaoGerenciador('Iniciar', width / 2, height / 2)
+
+  frameRate(40)
+  somDoJogo.loop();
+}
+
+function keyPressed() {
+  jogo.keyPressed(key)
+}
+
+function draw() {  
+  cenas[cenaAtual].draw()
+}
